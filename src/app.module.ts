f5df0c3b76e94726807f70de2c046feb502a2109
Gaurav Sekhri <gauravsekhri69@gmail.com>
2024-09-08 00:38:26 +0530

    implementation

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TRANSCODE_QUEUE } from './constants';
+import { TranscodeConsumer } from './transcode.consumer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({}),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: TRANSCODE_QUEUE,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, TranscodeConsumer],
+})
+export class AppModule {}
